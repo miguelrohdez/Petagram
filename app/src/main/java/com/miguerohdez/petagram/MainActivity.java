e78bbc48b223d4b795a400d73d89c68a9004c430
Miguel Rojas <crayzy_1_9@hotmail.com>
2020-08-07 01:01:12 -0500

    Creando Antes del refactor

--- app/src/main/java/com/miguerohdez/petagram/MainActivity.java
+++ app/src/main/java/com/miguerohdez/petagram/MainActivity.java
@@ -3,6 +3,12 @@ package com.miguerohdez.petagram;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void agregarFav(){
+        FloatingActionButton miFav = (FloatingActionButton) findViewById(R.id.btnFav);
+
+        miFav.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Toast.makeText(getBaseContext(), getResources().getString(R.string.action_btn), Toast.LENGTH_LONG).show();
+                Snackbar.make(v, getResources().getString(R.string.action_btn), Snackbar.LENGTH_LONG)
+                        .setAction(getResources().getString(R.string.app_name), new View.OnClickListener() {
+                            @Override
+                            public void onClick(View view) {
+                                Log.i("FuncionaSnack", "CLIKSNACK");
+                            }
+                        }) .show();
+                System.out.println("Funciona");
+
+            }
+        });
     }
 }
