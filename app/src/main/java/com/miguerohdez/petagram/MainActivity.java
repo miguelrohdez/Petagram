a3d6a4ee2df1a79f944da132c7f70cf147b0f124
Miguel Rojas <crayzy_1_9@hotmail.com>
2020-08-07 01:25:43 -0500

    Funcionando Floating

--- app/src/main/java/com/miguerohdez/petagram/MainActivity.java
+++ app/src/main/java/com/miguerohdez/petagram/MainActivity.java
@@ -16,25 +16,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-
-    public void agregarFav(){
         FloatingActionButton miFav = (FloatingActionButton) findViewById(R.id.btnFav);
 
         miFav.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //Toast.makeText(getBaseContext(), getResources().getString(R.string.action_btn), Toast.LENGTH_LONG).show();
-                Snackbar.make(v, getResources().getString(R.string.action_btn), Snackbar.LENGTH_LONG)
-                        .setAction(getResources().getString(R.string.app_name), new View.OnClickListener() {
-                            @Override
-                            public void onClick(View view) {
-                                Log.i("FuncionaSnack", "CLIKSNACK");
-                            }
-                        }) .show();
-                System.out.println("Funciona");
+                Toast.makeText(getBaseContext(), getResources().getString(R.string.action_btn), Toast.LENGTH_LONG).show();
+
 
             }
         });
+    }
+
+    public void agregarFav(){
+
     }
 }
